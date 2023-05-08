563076107c2338e4dd2e162bd18f87082fee694b
Anatoliy Yakovenko <yakovenko.anatoliy@gmail.com>
2023-05-08 15:58:33 +0300

    add MUI styles for buttons

---
+++ src/components/Counter/Counter.jsx
@@ -0,0 +1,22 @@
+import { useState } from "react";
+import { Button, ButtonGroup } from '@mui/material';
+import css from './Counter.module.css';
+
+
+export const Counter = () => {
+  const [value, setValue] = useState(42);
+
+  return (
+    <div className = {css.counterContainer}>
+      <p>Count {value}</p>
+      <ButtonGroup
+  disableElevation
+  variant="contained"
+  aria-label="Disabled elevation buttons"
+>
+  <Button onClick={() => setValue(value + 1)}>Increment</Button>
+  <Button onClick={() => setValue(value - 1)}>Decrement</Button>
+</ButtonGroup>
+    </div>
+  );
+};
