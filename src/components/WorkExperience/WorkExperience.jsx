21206bda2afba3f0b2fc9c8ea470e7381f369306
Anatoliy Yakovenko <yakovenko.anatoliy@gmail.com>
2023-05-08 17:19:21 +0300

    rendering WorkExperience with WorItem

--- src/components/WorkExperience/WorkExperience.jsx
+++ src/components/WorkExperience/WorkExperience.jsx
@@ -1,33 +1,46 @@
 import css from './WorkExperience.module.css';
+import {WorkItem} from '../WorkItem/WorkItem';
+
+
+const workExperienceList =[{
+    link: "https://deps.ua", 
+    name: "DEPS Ltd",
+    dates: "12/2021 – 05/2022", 
+    title: "Head of infrastructure solutions department", 
+    description: "Purchasing of equipment. Lunching a new products. Technical supporting", 
+},
+{
+    link: "https://romsat.ua", 
+    name: "ROMSAT Ltd",
+    dates: "06/2007 – 12/2021", 
+    title: "Head of cable systems direction", 
+    description: "Purchasing of equipment. Lunching a new products. Technical supporting",  
+},
+{
+    link: "https://ukrtelecom.ua", 
+    name: "Ukrtelecom",
+    dates: "07/2005 – 06/2007", 
+    title: "Engineer", 
+    description: "Installing and maintenance of cable systems",     
+}
+]
 
 export function WorkExperience(){
     return (
         <div className={css.work}>
-
         <h2 className={css.title}>Work Experience</h2>
-        
         <ul>
-            <li className={css.item}>
-                <p>Name of employer: <span>DEPS Ltd</span></p>
-                <p>Dates of employment: <span>12/2021 – 05/2022</span></p>
-                <p>Job title: <span>Head of infrastructure solutions
-                        department</span> </p>
-                <p>Project/Role description: <span>Purchasing of equipment. Lunching a new products.
-                        Technical supporting</span> </p>
-            </li>
-            <li className={css.item}>
-                <p>Name of employer: <span>ROMSAT Ltd</span> </p>
-                <p>Dates of employment: <span>06/2007 – 12/2021</span> </p>
-                <p>Job title: <span>Head of cable systems direction</span> </p>
-                <p>Project/Role description: <span>Purchasing of equipment. Lunching a new products.
-                        Technical supporting</span> </p>
-            </li>
-            <li className={css.item}>
-                <p>Name of employer: <span>Ukrtelecom</span> </p>
-                <p>Dates of employment: <span>07/2005 – 06/2007</span> </p>
-                <p>Job title: <span>Engineer</span></p>
-                <p>Project/Role description: <span>Installing and maintenance of cable systems</span></p>
-            </li>
+        {workExperienceList.map(({title, description, name, link, dates}, index)=>(
+         <WorkItem 
+         key = {index}
+         link = {link}
+         name ={name}
+         dates ={dates}
+         title ={title}
+         description ={description}
+        />
+        ))
+        } 
         </ul>
         </div>   
     );
